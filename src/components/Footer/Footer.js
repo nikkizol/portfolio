1f3d47208d020faff0bf7ea4fe41157e3c8ea8f9
MZ <nikita.zholkovskyi@gmail.com>
2020-12-26 16:58:01 +0100

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,58 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faLinkedin,
+  faInstagram,
+  faGithub,
+  f2b6,
+} from "@fortawesome/free-brands-svg-icons";
+
 import "./styles.css";
 
 function Footer() {
   return (
     <>
-      <h1>Contact</h1>
+      <div data-aos="fade-up ">
+        <div className="footer">
+          <div className="container footer-container">
+            <div class="row">
+              <div class="col">
+                <h4>Mykyta Zholkovskyi</h4>
+              </div>
+              <div class="col">
+                <a
+                  href="https://www.youtube.com/c/jamesqquick"
+                  className="youtube social"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a
+                  href="https://www.facebook.com/learnbuildteach/"
+                  className="facebook social"
+                >
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a
+                  href="https://www.twitter.com/jamesqquick"
+                  className="twitter social"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+                <a
+                  href="https://www.instagram.com/learnbuildteach"
+                  className="instagram social"
+                >
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+              </div>
+              <div class="col">
+                <h4>©2020</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
